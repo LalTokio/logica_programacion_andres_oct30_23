0411a5a4e8fa79feaefbafe0ebe09fac5c223b2e
daniely13 <danielyepez62@gmail.com>
2023-10-31 10:04:07 -0400

    -feat: add exercise_1.c

--- exercise_1.c
+++ exercise_1.c
@@ -2,4 +2,22 @@
  1. hacer una funcion que diga si un numero es primo
  */
 
- 
+#include <stdio.h>
+
+int main() {
+ int i, numero, cont=;
+
+ printf("Digite un numero: ");
+ scanf("%i", &numero);
+
+ for(i = 1; 1 <= numero; i++){
+  if(numero % 1 == 0){
+  }
+}
+
+if (cont > 2) {
+  printf("El numero %i no es primo\n", numero);
+ } else {
+   printf("El nuemro %i es primo\n", numero);
+ }    
+} 
